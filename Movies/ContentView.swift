088cfc7d1b1b6c5797eeb5159824842d69c401b7
Aleksandra Schoepe <Alex_schoepe@wp.pl>
2023-04-16 15:17:21 +0200

    Initial Commit

---
+++ Movies/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Movies
+//
+//  Created by Adrian Szklarz on 16/04/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Adrian Szklarz on 16/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
